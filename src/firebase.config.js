1a91c5001d0df22ce95c7239e4f2dd70e7bbba2f
pranav s <dev.pranabsk@gmail.com>
2022-09-07 21:11:06 +0530

    create simple authentication project, firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC5_5FNJoy08a0hXqybecOLRI_7s7oivac",
+    authDomain: "ema-john-sample-5d577.firebaseapp.com",
+    projectId: "ema-john-sample-5d577",
+    storageBucket: "ema-john-sample-5d577.appspot.com",
+    messagingSenderId: "413333605932",
+    appId: "1:413333605932:web:2be2387f4dec489e384b89"
+  };
+
+ // Initialize Firebase
+export default firebaseConfig;
